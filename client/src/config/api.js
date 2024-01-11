e7799e6129104ef9219f9329d1967d96bac0c222
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-01-11 11:56:57 +0530

    the sha

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,5 @@
 // src/config/api.js
-const API_BASE_URL = 'https://webwork.vercel.app';
+const API_BASE_URL = 'https://webworkserver-production-c9b2.up.railway.app/';
 
 export default {
   BASE_URL: API_BASE_URL,
