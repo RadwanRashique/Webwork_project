23730d5684f318133c1f63a3cf80c658d10fedde
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-01-11 07:21:03 +0530

    cors orgin set

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors");
 app.use(cors({
-  origin:"*"
+  origin: "https://webworkfront.vercel.app",
 }));
 // to use (.env file access)
 require('dotenv').config()
