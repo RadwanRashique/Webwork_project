db319b434cf342f8122b368ad8f17da81f4f6666
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-01-11 19:48:21 +0530

    rehosting

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,5 @@
 // src/config/api.js
-const API_BASE_URL = 'https://webworkserver-production-c9b2.up.railway.app/';
+const API_BASE_URL = 'https://webwork.website/';
 
 export default {
   BASE_URL: API_BASE_URL,
